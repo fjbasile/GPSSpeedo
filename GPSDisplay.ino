18c1247a1762621931266d9262d405bd5a95ee50
fjbasile <fjbasile@icloud.com>
2016-05-28 15:25:47 -0500

    Update GPSDisplay.ino

--- GPSDisplay.ino
+++ GPSDisplay.ino
@@ -334,6 +334,10 @@ void displaySats()
   s7sSendStringI2C("5t" + sats);
 }
 
+
+// This custom function works somewhat like a serial.print.
+//  You can send it an array of chars (string) and it'll print
+//  the first 4 characters in the array
 void s7sSendStringI2C(String toSend)
 {
   Wire.beginTransmission(s7sAddress);
@@ -344,6 +348,8 @@ void s7sSendStringI2C(String toSend)
   Wire.endTransmission();
 }
 
+// Send the clear display command (0x76)
+//  This will clear the display and reset the cursor
 void clearDisplayI2C()
 {
   Wire.beginTransmission(s7sAddress);
@@ -351,6 +357,10 @@ void clearDisplayI2C()
   Wire.endTransmission();
 }
 
+// Set the displays brightness. Should receive byte with the value
+//  to set the brightness to
+//  dimmest------------->brightest
+//     0--------127--------255
 void setBrightnessI2C(byte value)
 {
   Wire.beginTransmission(s7sAddress);
@@ -359,6 +369,10 @@ void setBrightnessI2C(byte value)
   Wire.endTransmission();
 }
 
+// Turn on any, none, or all of the decimals.
+//  The six lowest bits in the decimals parameter sets a decimal 
+//  (or colon, or apostrophe) on or off. A 1 indicates on, 0 off.
+//  [MSB] (X)(X)(Apos)(Colon)(Digit 4)(Digit 3)(Digit2)(Digit1)
 void setDecimalsI2C(byte decimals)
 {
   Wire.beginTransmission(s7sAddress);
